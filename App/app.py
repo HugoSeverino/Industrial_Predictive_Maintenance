758760b44f97b55dda57cd2767643535705e7b34
hugo77380 <hugo.severino@hei.fr>
2024-06-30 12:27:32 +0200

    Creating folder

---
+++ App/app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from dash import Dash, html
+import pandas as pd
+
+# Prétraitement des données
+data = pd.DataFrame({
+    "Category": ["A", "B", "C"],
+    "Values": [10, 20, 30]
+})
+
+# Créer une application Flask
+server = Flask(__name__)
+app = Dash(__name__, server=server, routes_pathname_prefix='/')
+
+app.layout = html.Div([
+    html.H1("Visualisation des Données"),
+    html.Div([
+        html.P("Voici un exemple de graphique"),
+        # Graphique ou autres composants Dash
+    ])
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
